2cf1da6a7de0235621fe68aac84c480806eee5ae
Aron Rissato <aron_rissato@hotmail.com>
2026-09-09 20:48:26 -0300

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if (s.Length != t.Length) {
+            return false;
+        }
+        Dictionary<char, int> countS = new Dictionary<char, int>();
+        Dictionary<char, int> countT = new Dictionary<char, int>();
+        for (int i = 0; i < s.Length; i++) {
+            countS[s[i]] = countS.GetValueOrDefault(s[i], 0) + 1;
+            countT[t[i]] = countT.GetValueOrDefault(t[i], 0) + 1;
+        }
+
+        foreach (var key in countS.Keys) {
+            if (!countT.ContainsKey(key) || countS[key] != countT[key]) {
+                return false;
+            }
+        } 
+        return true;
+    }
+}
